81c4ceb257b88b5e47a87c54c0c3d38e6e3abbae
Henry Miller <mail.henrym+git@gmail.com>
2022-01-11 14:36:25 -0500

    commit tree 5e984236921cbc6ec4b1c40bdfa02b1b81163617

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+//1 
+function findMatching(drivers, string) {
+    return drivers.filter(function(driver) { return driver.toLowerCase() === string.toLowerCase()}) 
+}
+
+//2 
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function(driver) { return driver.startsWith(string)}) 
+}
+
+//3 
+function matchName(drivers, string) {
+    return drivers.filter(function(driver) { return driver.name === string}) 
+}
